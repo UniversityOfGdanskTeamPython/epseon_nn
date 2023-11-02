66328850f7d59b234ff30e2beccae83d421a31e7
MatSzpy <79718688+MatSzpy@users.noreply.github.com>
2023-11-02 23:03:38 +0100

    Add configuration files for support tools (#4)
    Resolves #1
    Resolves #2
    Resolves #3
    
    ---------
    
    Co-authored-by: Mateusz Szpyruk <mateuszszpyruk@gmail.com>
    Co-authored-by: Krzysztof Wiśniewski <argmaster.world@gmail.com>

---
+++ src/pec_framework_data_consumer/__init__.py
@@ -0,0 +1,11 @@
+"""Package for PCB formats visualization."""
+from __future__ import annotations
+
+import sys
+
+__version__ = "0.1.0"
+
+
+def example() -> None:
+    """Say hello to world."""
+    sys.stdout.write("Hello world! \n")
