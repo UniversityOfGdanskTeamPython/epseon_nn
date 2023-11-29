d9082cab7942624e8c400e87d76c072a641c5f04
Mateusz Szpyruk <mateuszszpyruk@gmail.com>
2023-11-29 20:04:10 +0100

    Add small changes to json_data_loader and feed_forward.

--- src/epseon_nn/_epseon_nn/data_loaders/json_data_loader.py
+++ src/epseon_nn/_epseon_nn/data_loaders/json_data_loader.py
@@ -5,13 +5,43 @@ from typing import TYPE_CHECKING
 
 from epseon_nn._epseon_nn.data_loaders.data_loader import DataLoader
 
+from pathlib import Path
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
 if TYPE_CHECKING:
     import torch
-
-
 class JsonDataLoader(DataLoader):
-    """Loader for JSON data."""
+
+    def __init__(self, directory: str) -> None:
+        self.this_dir = Path(__file__).parent / "networks" / "data"
 
     def get_training_data(self) -> torch.Tensor:
+        data = []
+        for files in self.this_dir.iterdir():
+            if files.is_file() and file.suffix == ".json":
+                with file.open() as file:
+                    data = json.load(file)
+        return data
+
+
+
+    def process_data(self) -> tuple[TensorDataset, TensorDataset]:
         """Return data loaded from some source."""
-        ...
+        x = torch.Tensor(get_training_data(self.this_dir)).reshape(1, 41)
+        y = torch.Tensor(JsonDataLoader(self.this_dir.parent).get_training_data())
+        scaler = StandardScaler().fit(y.reshape(41, 1))
+        y = torch.Tensor(scaler.transform(y.reshape(41, 1)))
+
+        x_train, x_test, y_train, y_test = train_test_split(
+            x,
+            y,
+            train_size=0.8,
+            random_state=42,
+        )
+
+        train_data = TensorDataset(x_train, y_train)
+        test_data = TensorDataset(x_test, y_test)
+
+        return train_data, test_data
--- src/epseon_nn/_epseon_nn/train/feed_forward.py
+++ src/epseon_nn/_epseon_nn/train/feed_forward.py
@@ -5,7 +5,7 @@ from epseon_nn._epseon_nn.data_loaders.data_loader import DataLoader
 from epseon_nn._epseon_nn.data_loaders.json_data_loader import JsonDataLoader
 from epseon_nn._epseon_nn.models.feed_forward import FeedForwardModel
 
-
+from tensorflow.keras.losses import MeanSquaredError
 class TrainFeedForwardModel:
     """Code for training FeedForwardModel network."""
 
@@ -23,16 +23,27 @@ class TrainFeedForwardModel:
         """Train neural network."""
         self.load_train_data()
         self.create_model()
-        ...
+        self.network.compile(loss=MeanSquaredError, optimizer='adam', metrics=['accuracy'])
+        self.network.fit(self.train_data[0], self.train_data[1], epochs=10, batch_size=32, validation_split=0.3)
 
     def save_model(self) -> None:
         """Save trained model to file."""
-        ...
+        weights = model.get_weights()
+        with open('weights.txt', 'w') as f:
+            for w in weights:
+                f.write(str(w) + '\n')
+
+    def validate_network(self) -> None:
+        """Validate neural network."""
+        self.load_train_data()
+        self.create_model()
+        self.network.compile(loss=MeanSquaredError(), optimizer='adam', metrics=['accuracy'])
+        self.network.evaluate(self.train_data[0], self.train_data[1])
 
 
 def train_network() -> None:
     """Train FeedForwardModel neural network."""
     trainer = TrainFeedForwardModel(JsonDataLoader())
     trainer.train_network()
-    # maybe some validation code?
+    trainer.validate_network()
     trainer.save_model()
